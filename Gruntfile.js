bc39338eee529186d2119a13f9c22fabe127b279
Eli Sagy <sagyeli@gmail.com>
2015-02-09 03:47:42 +0200

    Fixing some problems with configuration files regarding MongoDB connection with Mongoose

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2015-01-09 using generator-angular-fullstack 2.0.13
+// Generated on 2015-02-09 using generator-angular-fullstack 2.0.13
 'use strict';
 
 module.exports = function (grunt) {
--- server/routes.js
+++ server/routes.js
@@ -6,15 +6,10 @@
 
 var errors = require('./components/errors');
 
-var bitcoinapi = require('bitcoin-node-api');
-bitcoinapi.setWalletDetails({ host: 'localhost', port: 8332, user: 'admin', pass: '1234' });
-bitcoinapi.setAccess('default-safe');
-
 module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
-  app.use('/bitcoin/api', bitcoinapi.app);
   
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
