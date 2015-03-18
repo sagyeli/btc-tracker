aad61bfb993682c8b2020d6903986d717d3ac797
Eli Sagy <sagyeli@gmail.com>
2015-03-18 22:42:29 +0200

    Creating blocks and transactions sychronized without using address object (for now)

--- server/config/seed.js
+++ server/config/seed.js
@@ -13,8 +13,8 @@ var Transaction = require('../api/transaction/transaction.model');
 var Address = require('../api/address/address.model');
 
 
-var transactionRecordCreator = function(txid, block) {
-  bitcoinClient.cmd([{ method: 'getrawtransaction', params: [txid, 1] }], function(err, response) {
+var transactionsRecordCreator = function(txids, block, numOfTxsLeft, nextBlockCallback) {
+  bitcoinClient.cmd([{ method: 'getrawtransaction', params: [txids[txids.length - numOfTxsLeft], 1] }], function(err, response) {
     if (err) {
       console.error(err);
       return;
@@ -31,6 +31,13 @@ var transactionRecordCreator = function(txid, block) {
       if (err) {
         console.error(err);
         return;
+      }
+
+      if (numOfTxsLeft > 1) {
+        transactionsRecordCreator(txids, block, numOfTxsLeft - 1, nextBlockCallback);
+      }
+      else if (nextBlockCallback) {
+        nextBlockCallback();
       }
     });
   });
@@ -57,12 +64,25 @@ var blockRecordCreator = function(firstBlockIndex, lastBlockIndex) {
           return;
         }
 
-        for (var i = 0 ; response.tx && i < response.tx.length ; i++) {
-          transactionRecordCreator(response.tx[i], block);
-        }
+        if (firstBlockIndex === 0) {
+          Transaction.create({
+            hash: '4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b',
+            block: block,
+            input: [],
+            output: [{
+              address: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+              amount: 50
+            }]
+          });
 
-        if (firstBlockIndex < lastBlockIndex) {
-          blockRecordCreator(firstBlockIndex + 1, lastBlockIndex);
+          blockRecordCreator(1, lastBlockIndex);
+        }
+        else if (response.tx.length > 0) {
+          transactionsRecordCreator(response.tx, block, response.tx.length, function () {
+            if (firstBlockIndex < lastBlockIndex) {
+              blockRecordCreator(firstBlockIndex + 1, lastBlockIndex);
+            }
+          });
         }
       });
     });
